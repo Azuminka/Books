7f45dd13575752f15c1247eb5045162f91a7aa11
Azuminka <peckovastanka@gmail.com>
2022-05-12 22:17:44 +0200

    adding exceptions and author and book entity

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/author/data/AuthorEntity.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/author/data/AuthorEntity.java
@@ -1,4 +1,21 @@
 package sk.stuba.fei.uim.oop.assignment3.author.data;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+import sk.stuba.fei.uim.oop.assignment3.book.data.BookEntity;
+
+@Entity
+@Getter
+@Setter
 public class AuthorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    @OneToMany (orphanRemoval = true)
+    private List<BookEntity> books;
 }
--- src/main/java/sk/stuba/fei/uim/oop/assignment3/book/data/BookEntity.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/book/data/BookEntity.java
@@ -1,4 +1,23 @@
 package sk.stuba.fei.uim.oop.assignment3.book.data;
 
+import lombok.Getter;
+import lombok.Setter;
+import sk.stuba.fei.uim.oop.assignment3.author.data.AuthorEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToOne
+    private AuthorEntity author;
+    private int pages;
+    private int amount;
+    private int lendCount;
 }
