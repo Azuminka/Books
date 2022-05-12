acb9a5f33af38cb943bd6c0d7226fffa220702bc
Azuminka <peckovastanka@gmail.com>
2022-05-12 21:45:16 +0200

    project structure

---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/author/data/IAuthorRepository.java
@@ -0,0 +1,4 @@
+package sk.stuba.fei.uim.oop.assignment3.author.data;
+
+public interface IAuthorRepository {
+}
---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/list/web/BookListController.java
@@ -0,0 +1,4 @@
+package sk.stuba.fei.uim.oop.assignment3.list.web;
+
+public class BookListController {
+}
